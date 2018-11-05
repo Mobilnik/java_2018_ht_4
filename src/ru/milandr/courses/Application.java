6d1f859f709eedfa8a4af164b8de76fbb64c6033
Alexandr Bolodurin <36509244+Bolodurin@users.noreply.github.com>
2018-11-05 10:05:09 +0300

    upd app

--- src/ru/milandr/courses/Application.java
+++ src/ru/milandr/courses/Application.java
@@ -1,7 +1,26 @@
 package ru.milandr.courses;
 
-public class Application {
+import ru.milandr.courses.Tasks;
+
+import java.sql.*;
+
+public class Application  {
     public static void main(String[] args) {
-        System.out.println("Hello. This is Home Task #4");
+        //System.out.println("Hello. This is Home Task #4");
+        Tasks tasks = new Tasks();
+        try{tasks.task1();}
+        catch (SQLException exc){exc.printStackTrace();}
+
+        try{tasks.task2();}
+        catch (SQLException exc){exc.printStackTrace();}
+
+        try{tasks.task3();}
+        catch (SQLException exc){exc.printStackTrace();}
+
+        try{tasks.task4();}
+        catch (SQLException exc){exc.printStackTrace();}
+
+        try{tasks.task5();}
+        catch (SQLException exc){exc.printStackTrace();}
     }
 }
